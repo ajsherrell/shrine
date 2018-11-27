8360311e12f2f6f30db8617ddd59da3ff11d4f88
AJ Sherrell <amiesherrell@gmail.com>
2018-11-27 17:25:36 -0500

    add password validation

--- app/src/main/java/com/google/codelabs/mdc/java/shrine/LoginFragment.java
+++ app/src/main/java/com/google/codelabs/mdc/java/shrine/LoginFragment.java
@@ -2,7 +2,9 @@ package com.google.codelabs.mdc.java.shrine;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,4 +26,11 @@ public class LoginFragment extends Fragment {
     }
 
     // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
+    /*
+   In reality, this will have more complex logic including, but not limited to, actual
+   authentication of the username and password.
+*/
+    private boolean isPasswordValid(@Nullable Editable text) {
+        return text != null && text.length() >= 8;
+    }
 }
